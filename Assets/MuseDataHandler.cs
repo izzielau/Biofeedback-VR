305f8d66f5b0a8fd999074b36878c2e7180bcf71
joshuayang <josh.ym3@gmail.com>
2018-09-29 18:42:25 -0700

    Initial commit with working data transfer from Muse headband to Unity.

---
+++ Assets/MuseDataHandler.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Created by Josh. Handles EEG Data recieved from MuseIO.
+ *
+ *
+ * INSTRUCTIONS to set up environment:
+ * 1. Go to http://developer.choosemuse.com/tools/mac-tools/muselab and download and install MuseIO and MuseLab.
+ * 2. Connect Muse headband to computer via bluetooth.
+ * 3. Run this command in terminal: muse-io --device Muse-XXXX --osc osc.udp://localhost:5000, where Muse-XXXX is your muse device name.
+ * 4. DON'T open port 5000 on MuseLab or else it will interfere with the OSC server generated here. Simply run this code, and the server
+ *    should be recieving the EEG data.
+ *
+ */
+
+public class MuseDataHandler : MonoBehaviour {
+
+	public OSC osc;
+	static float museDataVar = 0;
+	static float normalizedMuseDataVar = 0;
+
+	private static float maxVal = 1000f;
+	private static float minVal = 0;
+
+
+	void Start () {
+		// Currently listens for general eeg data values.
+		// Other data can be listened to through addresses found here: http://developer.choosemuse.com/tools/available-data
+		osc.SetAddressHandler("/muse/eeg", OnReceiveEEG);
+	}
+
+	void OnReceiveEEG(OscMessage message) {
+		// Get EEG data from message
+		museDataVar = message.GetFloat(0);
+
+		// Update min and max values if needed
+		if (museDataVar < minVal) {
+			minVal = museDataVar;
+		}
+		if (museDataVar > maxVal) {
+			maxVal = museDataVar;
+		}
+
+		// Set normalized data variable
+		normalizedMuseDataVar = Normalize(museDataVar, minVal, maxVal);
+	}
+
+    /* Normalizes input EEG data to spit out a number between 1 and 0 */
+	float Normalize (float val, float min, float max) {
+		return (val - min) / (max - min);
+	}
+
+	public float GetEEGData() {
+		return museDataVar;
+	}
+
+	public float GetNormalizedEEGData() {
+		return normalizedMuseDataVar;
+	}
+
+}
---
+++ Assets/SphereController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+/* Created by Josh. Controls scale and color of an experimental sphere based on the user's EEG data. */
+public class SphereController : MonoBehaviour {
+	public MuseDataHandler M;
+	public Text EEGText;
+	float EEGDataVal;
+	float EEGBufferedDataVal;
+
+	float bufferDecrease;
+
+	private float sphereScale;
+
+	// Use this for initialization
+	void Start () {
+		sphereScale = 1;
+		EEGDataVal = 0;
+		EEGBufferedDataVal = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		RecieveAndBufferData();
+		DisplayData();
+		UpdateSphereScale();
+		UpdateSphereColor();
+	}
+
+	void RecieveAndBufferData() {
+		try {
+			// Store data from muse data handler into local variable
+			EEGDataVal = M.GetNormalizedEEGData();
+		} catch (Exception e) {
+			Debug.LogWarning("Failed to read EEG data.");
+			Debug.LogWarning(e);
+		}
+
+		// Adjust the buffer based on the value of EEGDataVal
+		if (EEGDataVal > EEGBufferedDataVal) {
+
+			EEGBufferedDataVal = EEGDataVal;
+
+			// Reset buffer decrease
+			bufferDecrease = 0.005f;
+
+		} else if (EEGDataVal < EEGBufferedDataVal) {
+
+			// Decrement by buffer decrease each loop
+			EEGBufferedDataVal -= bufferDecrease;
+
+			// Exponentially increase buffer decrease
+			bufferDecrease *= 1.2f;
+		}
+	}
+
+	void DisplayData() {
+		try {
+			if (EEGText) {
+				EEGText.text = "EEG: " + M.GetEEGData();
+			}
+		} catch (Exception e) {
+		}
+	}
+
+	void UpdateSphereScale() {
+		// Random scaled value
+		float newScale = 1f + EEGBufferedDataVal * 2f;
+
+		// Set new scale
+		this.transform.localScale = new Vector3(newScale, newScale, newScale);
+	}
+
+	void UpdateSphereColor() {
+		//Fetch the Renderer from the GameObject
+        Renderer rend = this.GetComponent<Renderer>();
+
+		Color c = new Color(EEGBufferedDataVal / 2f, EEGBufferedDataVal / 2f, 0.6f + EEGBufferedDataVal / 2f, 1);
+        //Set the main Color of the Material
+        rend.material.color = c;
+	}
+}
